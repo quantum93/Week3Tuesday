f1b1b5da7e817e68c4c84b1cc6aee8504e0cfffe
Tae Lee <taebumlee@gmail.com>
2019-06-11 21:13:48 -0700

    this is update after refactoring process.

--- LeapYear/js/scripts.js
+++ LeapYear/js/scripts.js
@@ -1,5 +1,9 @@
 var leapYear = function(year) {
-  return false;
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 $(document).ready(function() {
